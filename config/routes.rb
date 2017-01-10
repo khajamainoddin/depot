e04af44bc343b66e289cb4e1b8b7ceeb0b5caf57
khajamainoddin <khajamainoddin.mohd@gmail.com>
2017-01-10 12:01:42 +0530

    added internalization

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,13 @@ delete 'logout' => :destroy
 
 end
 
-  resources :users
+  
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
+  root 'store#index', as: 'store_index', via: :all
 
+  resources :users
   resources :products do
 get :who_bought, on: :member
 end
